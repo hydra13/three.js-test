115da9a88e2ab07acc567d3c9aac7a3380d398bc
hydra13 <rob.sabirov@yandex.ru>
2019-10-10 08:09:27 +0300

    added a box texture

--- source.js
+++ source.js
@@ -1,21 +1,29 @@
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-);
-camera.position.z = 5;
+let scene, camera, renderer, cube;
+function init() {
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
+    );
+    camera.position.z = 5;
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
-const cube = new THREE.Mesh(geometry, material);
+    const geometry = new THREE.BoxGeometry(2, 2, 2);
+    // material - a simple blue color
+    // const material = new THREE.MeshBasicMaterial({ color: 0x0000ff });
 
-scene.add(cube);
+    // material - a box texture
+    const texture = new THREE.TextureLoader().load('textures/box.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    cube = new THREE.Mesh(geometry, material);
+
+    scene.add(cube);
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -24,4 +32,13 @@ function animate() {
     renderer.render(scene, camera);
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
